3b08de4fd63081478c9bb6b694ebccdae3f5d6a4
Matt DeKrey <mattdekrey@gmail.com>
2016-05-21 15:20:53 -0500

    Update licenses and Nuget metadata

---
+++ package.ps1
@@ -0,0 +1,6 @@
+dnu pack --configuration Release --out artifacts `
+    .\DeKreyConsulting.AdoTestability\ `
+    .\DeKreyConsulting.AdoTestability.Testing.Moq\ `
+    .\DeKreyConsulting.AdoTestability.Testing.SqlServer\ `
+    .\DeKreyConsulting.AdoTestability.Testing.Stubs\ `
+    
