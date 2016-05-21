697abb78ba218866ff628b9341197b9564a9585b
Matt DeKrey <mattdekrey@gmail.com>
2016-05-21 08:08:09 -0500

    Add documentation and a few features to flesh out the command builder

--- DeKreyConsulting.AdoTestability/CommandBuilder.cs
+++ DeKreyConsulting.AdoTestability/CommandBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,22 +12,64 @@ namespace DeKreyConsulting.AdoTestability
     using IReadOnlyParameterInitializers = IReadOnlyDictionary<string, ParameterInitializer>;
     using ReadOnlyParameterInitializers = ReadOnlyDictionary<string, ParameterInitializer>;
 
+    /// <summary>
+    /// Encapsulates the basics of building an ADO.Net command in a functional way. The CommandBuilders should be constructed statically and re-used across
+    /// multiple connections; it encapsulates connection-agnostic portions of the command as an object that can be re-used and builds a command given 
+    /// instance-specific values.
+    /// </summary>
     public class CommandBuilder
     {
-        public CommandBuilder(string commandText, IReadOnlyParameterInitializers parameters)
+        /// <summary>
+        /// Constructs the command builder.
+        /// </summary>
+        /// <param name="commandText">The command to execute, such as raw SQL or a stored procedure, depending on the command type</param>
+        /// <param name="parameters">A list of parameter initializers, by name, to set types, directions, etc. This is copied to prevent modification in order to ensure
+        /// deterministic behavior. (Slight performance hit, but these should be constructed statically.)</param>
+        /// <param name="commandType">The type of the command</param>
+        /// <param name="commandTimeout">The timeout duration, in seconds. Defaults to no timeout.</param>
+        public CommandBuilder(string commandText, IReadOnlyParameterInitializers parameters, CommandType commandType = CommandType.Text, int commandTimeout = 0)
         {
             this.CommandText = commandText;
-            this.Parameters = new ReadOnlyParameterInitializers(parameters.ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
+            this.CommandType = commandType;
+            this.CommandTimeout = commandTimeout;
+            this.Parameters = parameters;
         }
 
+        /// <summary>
+        /// Gets the command text specified by the constructor
+        /// </summary>
         public string CommandText { get; }
+
+        /// <summary>
+        /// Gets the command type specified by the constructor
+        /// </summary>
+        public CommandType CommandType { get; }
+
+        /// <summary>
+        /// Gets the command timeout specified by the constructor
+        /// </summary>
+        public int CommandTimeout { get; }
+
+        /// <summary>
+        /// Gets a copy of the parameters specified by the constructor
+        /// </summary>
         public IReadOnlyParameterInitializers Parameters { get; }
 
-        public DbCommand BuildFrom(DbConnection connection, IReadOnlyDictionary<string, object> parameterValues)
+        /// <summary>
+        /// Builds a command given specific values
+        /// </summary>
+        /// <param name="connection">The connection that the command will be executed against</param>
+        /// <param name="parameterValues">The values of the parameters to be used, if any</param>
+        /// <param name="transaction">The transaction within which to execute the command</param>
+        /// <returns>A new DbCommand that should be disposed of after use</returns>
+        public DbCommand BuildFrom(DbConnection connection, IReadOnlyDictionary<string, object> parameterValues = null, DbTransaction transaction = null)
         {
             var command = connection.CreateCommand();
             command.CommandText = CommandText;
+            command.CommandType = CommandType;
+            command.CommandTimeout = CommandTimeout;
             command.Connection = connection;
+            command.Transaction = transaction;
 
             foreach (var param in Parameters)
             {
@@ -38,13 +81,18 @@ namespace DeKreyConsulting.AdoTestability
 
             if (parameterValues != null)
             {
-                foreach (var param in parameterValues)
-                {
-                    command.Parameters[param.Key].Value = param.Value;
-                }
+                ApplyParameters(command, parameterValues);
             }
 
             return command;
+        }
+
+        public static void ApplyParameters(DbCommand command, IReadOnlyDictionary<string, object> parameterValues)
+        {
+            foreach (var param in parameterValues)
+            {
+                command.Parameters[param.Key].Value = param.Value;
+            }
         }
 
         public static CommandBuilder Construct<TParameter>(string commandText, IReadOnlyDictionary<string, Action<TParameter>> parameters)
--- DeKreyConsulting.AdoTestability/DbProviderFactoryExtensions.cs
+++ DeKreyConsulting.AdoTestability/DbProviderFactoryExtensions.cs
@@ -5,8 +5,17 @@ using System.Threading.Tasks;
 
 namespace System.Data.Common
 {
-    public static class DbProviderFactoryExtensions
+    /// <summary>
+    /// Extensions for the DbProviderFactory
+    /// </summary>
+    public static class AdoTestingDbProviderFactoryExtensions
     {
+        /// <summary>
+        /// Creates a connection with a specific connection string. Does not open the connection.
+        /// </summary>
+        /// <param name="providerFactory">A DbProviderFactory. Learn about DbProviderFactories from ADO.Net!</param>
+        /// <param name="connectionString">The connection string to use for the connection</param>
+        /// <returns>A new DbConnection</returns>
         public static DbConnection CreateConnection(this DbProviderFactory providerFactory, string connectionString)
         {
             var connection = providerFactory.CreateConnection();
