dc1c83a095d738490e30e5c9538de63a75c6e916
Matt DeKrey <mattdekrey@gmail.com>
2016-08-24 22:11:53 -0500

    Updates to ignores and deployment

--- package.ps1
+++ package.ps1
@@ -1,6 +1,4 @@
-dnu pack --configuration Release --out artifacts `
-    .\DeKreyConsulting.AdoTestability\ `
-    .\DeKreyConsulting.AdoTestability.Testing.Moq\ `
-    .\DeKreyConsulting.AdoTestability.Testing.SqlServer\ `
-    .\DeKreyConsulting.AdoTestability.Testing.Stubs\ `
-    
+dotnet pack .\DeKreyConsulting.AdoTestability\                   --configuration Release --output artifacts 
+dotnet pack .\DeKreyConsulting.AdoTestability.Testing.Moq\       --configuration Release --output artifacts
+dotnet pack .\DeKreyConsulting.AdoTestability.Testing.SqlServer\ --configuration Release --output artifacts
+dotnet pack .\DeKreyConsulting.AdoTestability.Testing.Stubs\     --configuration Release --output artifacts
